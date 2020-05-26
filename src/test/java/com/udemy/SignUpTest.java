399eb04f5a4d1185b1d26418aad270d2ee97b46b
NaumLiuda <lsmog07@gmail.com>
2020-05-27 00:11:55 +0300

    added locators

--- src/test/java/com/udemy/SignUpTest.java
+++ src/test/java/com/udemy/SignUpTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -15,6 +17,12 @@ public class SignUpTest {
 
     @Test(description = "Sign Up with valid credential")
     public void signUpWithValidCredentials() {
+        By signUpPopUpBtnLocator = By.xpath("(//button[@class='btn btn-primary'])[1]");
+        By fullNameFieldLocator = By.id("id_fullname");
+        By emailFieldLocator = By.id("email--1");
+        By pwFieldLocator = By.id("password");
+        By userInitialsLocator = By.xpath("//span[@class='user-initials']");
+        By submitBtnLocator = By.xpath("//input[@id='submit-id-submit']");
 
         File chromeDriver = new File("src/main/resources/chromedriver.exe");
         ChromeDriverService chromeService = new ChromeDriverService.Builder()
@@ -23,37 +31,41 @@ public class SignUpTest {
                 .build();
 
         WebDriver driver = new ChromeDriver(chromeService);
+        WebDriverWait wait = new WebDriverWait(driver, 5);
 
         driver.get("https://www.udemy.com/");
-        pause(4000);
 
-
-        WebElement signUpPopUpBtn = driver.findElement(
-                By.xpath("(//button[@class='btn btn-primary'])[1]"));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(signUpPopUpBtnLocator));
+        WebElement signUpPopUpBtn = driver.findElement(signUpPopUpBtnLocator);
         signUpPopUpBtn.click();
-        pause(5000);
 
-        WebElement fullNameField = driver.findElement(By.id("id_fullname"));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(fullNameFieldLocator));
+        WebElement fullNameField = driver.findElement(fullNameFieldLocator);
         fullNameField.clear();
         fullNameField.sendKeys("Nester Test");
 
-        WebElement emailField = driver.findElement(By.id("email--1"));
+        WebElement emailField = driver.findElement(emailFieldLocator);
         emailField.clear();
-        emailField.sendKeys("test.nester+1@gmail.com");
+        emailField.sendKeys("test.nester@gmail.com");
 
-        WebElement pwField = driver.findElement(By.id("password"));
+        WebElement pwField = driver.findElement(pwFieldLocator);
         pwField.clear();
         pwField.sendKeys("147852Zx!");
+
 
         WebElement submitBtn = driver.findElement(By.xpath("//input[@id='submit-id-submit']"));
         submitBtn.click();
 
-        pause(4000);
 
-        WebElement userInitials = driver.findElement(By.xpath("//span[@class='user-initials']"));
+        WebElement submitBtn = driver.findElement(submitBtnLocator);
+        wait.until(ExpectedConditions.elementToBeClickable(submitBtnLocator));
+        submitBtn.click();
+
+
+        WebElement userInitials = driver.findElement(userInitialsLocator);
+        pause(5000);
         Assert.assertEquals(userInitials.getText(),"NT");
 
-        pause(5000);
         driver.quit();
     }
     private void pause(int milis){
